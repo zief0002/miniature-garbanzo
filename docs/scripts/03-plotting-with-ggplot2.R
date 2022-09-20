d0114f5d50694cdec593404684553c185643612a
Andrew Zieffler <zief0002@umn.edu>
2022-09-19 19:43:45 -0500

    Added answer keys for worksheet 1 and 3

--- docs/scripts/03-plotting-with-ggplot2.R
+++ docs/scripts/03-plotting-with-ggplot2.R
@@ -5,268 +5,37 @@
 library(dplyr)
 library(ggplot2)
 library(readr)
-
-
-
-##################################################
-### Read in data
-##################################################
-
-city = read_csv("~/Documents/github/epsy-8251/data/riverview.csv")
-
-head(city)
-
-
-
-##################################################
-### Understanding the basic syntax
-##################################################
-
-ggplot(data = city, aes(x = education, y = income)) + 
-  geom_point() +
-  theme_bw()
-
-
-
-##################################################
-### Adding layers
-##################################################
-
-ggplot(data = city, aes(x = education, y = income)) + 
-  geom_point() + 
-  geom_smooth()
-
-
-
-##################################################
-### Good syntactic habits
-##################################################
-
-ggplot(data = city, aes(x = education, y = income)) +
-  geom_point() +
-  geom_smooth() +
-  theme_bw()
-
-
-
-##################################################
-### Global vs local aesthetics
-##################################################
-
-# Global aesthetics
-ggplot(data = city, aes(x = education, y = income, color = gender)) +
-  geom_point() +
-  geom_smooth()
-
-
-# local aesthetics
-ggplot(data = city, aes(x = education, y = income)) +
-  geom_point(aes(color = gender)) +
-  geom_smooth()
-
-
-
-##################################################
-### Fixed aesthetics
-##################################################
-
-ggplot(data = city, aes(x = education, y = income)) + 
-  geom_point(color = "red", size = 4) +
-  theme_bw()
-
-
-
-##################################################
-### Your Turn 
-##################################################
-
-ggplot(data = city, aes(x = education, y = income, color = gender)) +
-  geom_point() +
-  geom_smooth(color = "yellow", fill = "darkblue")
-
-
-
-
-##################################################
-### Useful point aesthetics
-##################################################
-
-ggplot(data = city, aes(x = education, y = income)) +
-  geom_point(aes(color = party, shape = gender), size = 4)
-
-
-
-##################################################
-### Faceting: Separate plots for subgroups
-##################################################
-
-ggplot(data = city, aes(x = education, y = income)) +
-  geom_point() +
-  facet_wrap(~ party, nrow = 2)
-
-
-
-##################################################
-### Faceting on multiple variables
-##################################################
-
-ggplot(data = city, aes(x = education, y = income)) +
-  geom_point() +
-  facet_grid(party ~ gender)
-
-
-
-##################################################
-### Changing the axis label
-##################################################
-
-ggplot(data = city, aes(x = education, y = income)) +
-  geom_point() +
-  xlab("Education level (in years)") +
-  ylab("Income level (in U.S. dollars)")
-
-
-
-##################################################
-### Changing the axis limits
-##################################################
-
-ggplot(data = city, aes(x = education, y = income)) +
-  geom_point(size = 4) +
-  xlab("Education level (in years)") +
-  ylab("Income level (in U.S. dollars)") +
-  xlim(0, 30) +
-  ylim(0, 100000)
-
-
-
-##################################################
-### Fine-tuning the axis scales
-##################################################
-
-ggplot(data = city, aes(x = education, y = income)) +
-  geom_point() +
-  scale_x_continuous(
-    name = "Education level (in years)",
-    breaks = c(10, 12, 14, 16, 18, 20, 22, 24)
-  )
-
-
-
-##################################################
-### Customizing the color
-##################################################
-
-ggplot(data = city, aes(x = seniority, y = education)) +
-  geom_point(aes(color = party)) +
-  scale_color_manual(
-    values = c("#00afbb", "#e7b800", "#fc4e07")
-  )
-
-
-
-##################################################
-### Customizing the legend
-##################################################
-
-ggplot(data = city, aes(x = seniority, y = education)) +
-  geom_point(aes(color = party)) +
-  scale_color_manual(
-    name = "Political affiliation",
-    values = c("#00afbb", "#e7b800", "#fc4e07"),
-    labels = c("Dem", "Ind", "Rep")
-  )
-
-
-
-##################################################
-### Greyscale palette
-##################################################
-
-ggplot(data = city, aes(x = education, y = income)) +
-  geom_point(aes(color = party)) +
-  scale_color_grey(
-    name = "Political affiliation"
-  )
-
-
-
-##################################################
-### Color brewer palette
-##################################################
-
-ggplot(data = city, aes(x = education, y = income)) +
-  geom_point(aes(color = party)) +
-  scale_color_brewer(
-    name = "Political affiliation",
-    palette = "Set2"
-  )
-
-
-
-##################################################
-### Themes
-##################################################
-
-ggplot(data = city, aes(x = education, y = income)) +
-  geom_point() +
-  theme_bw()
-
-
-
-##################################################
-### Fine-tuning the theme
-##################################################
-
-ggplot(data = city, aes(x = education, y = income)) +
-  geom_point() +
-  theme_bw() +
-  theme(
-    axis.title.x = element_text(face = "bold", color = "blue"),
-    axis.title.y = element_text(face = "italic")
-  )
-
-
-
-##################################################
-### The ggthemes package
-##################################################
-
-# Load ggthemes package
-library(ggthemes)
-
-# Plot with Wall Street Journal theme
-ggplot(data = city, aes(x = education, y = income)) +
-  geom_point() +
-  theme_wsj()
-
-
-
-##################################################
-### The scales package
-##################################################
-
-# Load the scales package
 library(scales)
 
-# Plot with dollar signs on the Y labels
-ggplot(data = city, aes(x = education, y = income)) +
-  geom_point() +
-  scale_y_continuous(
-    name = "Annual income",
-    labels = dollar
-  )
+
+
+##################################################
+### Task 1: Import data
+##################################################
+
+city = read_csv("https://raw.githubusercontent.com/zief0002/miniature-garbanzo/main/data/riverview.csv")
+
+# View data
+city
 
 
 
 ##################################################
-### Your turn
+### Task 2: Sketch
 ##################################################
 
-# Load scales library
-library(scales) # To format $ in the labels
+# Hopefully you have sketched a scatterplot (points) with education on the x-axis, 
+# income on the y-axis, and x- and y-axis labels.
+# theme_bw() is a black-and-white theme
 
-# Plot
+
+
+##################################################
+### Task 3: Mimic a Plot
+##################################################
+
+# Note that I use the label_number() function from the {scales} package to format the income labels
+
 ggplot(data = city, aes(x = education, y = income)) +
   geom_point(aes(color = party), size = 4) +
   scale_color_manual(
@@ -276,19 +45,97 @@ ggplot(data = city, aes(x = education, y = income)) +
   xlab("Education level (in years)") +
   scale_y_continuous(
     name = "Annual income (in U.S. dollars)", 
-    labels = dollar
+    labels = label_number(prefix = "$", suffix = "k")
   ) +
   theme_bw() +
   facet_wrap(~ gender)
 
 
+##################################################
+### Task 4: Export a Plot and Import it into a Document
+##################################################
+
+# Use the export button in the plot tab to export the plot to a PNG
+# Change the width and height so that the plot is not too stretched out nor too squished
+
+
 
 ##################################################
-### Combining ggplot with dplyr
+### Task 5: Mimic a Plot
 ##################################################
 
-city %>%
-  filter(gender == "female") %>%
-  ggplot(aes(x = education, y = income)) +
-  geom_point()
+ggplot(data = city, aes(x = income)) +
+  geom_histogram(color = "black", fill = "yellow") +
+  scale_x_continuous(
+    name = "Annual income (in U.S. dollars)", 
+    labels = label_number(prefix = "$", suffix = "k")
+  ) +
+  ylab("Frequency") +
+  theme_bw()
+
+
+
+##################################################
+### Task 6: Mimic a Plot
+##################################################
+
+# Import data
+gap = read_csv("https://raw.githubusercontent.com/zief0002/miniature-garbanzo/main/data/gapminder.csv")
+
+
+# Create plot
+ggplot(data = gap, aes(x = income, y = life_exp)) +  
+  geom_point(shape = 21, aes(fill = region, size = population)) +
+  scale_x_continuous(
+    name = "Per-person income (in international dollars)", 
+    # breaks = c(0.5, 1, 2, 4, 8, 16, 32, 64, 128),
+    labels = label_number(prefix = "$", suffix = "k")
+  ) +
+  scale_y_continuous(
+    name = "Life expectancy (in years)"
+  ) +
+  scale_fill_manual(
+    name = "World Region",
+    values = c("#87CEEB", "#DDA0DD", "#FFA500", "#7FFF00"),
+    labels = c("Africa", "The Americas", "Asia", "Europe")
+  ) +
+  scale_radius(range = c(1, 15)) +
+  theme_minimal() +
+  guides(size = "none")
+
+
+
+
+##################################################
+### Task 7: Add Text to Label a Point
+##################################################
+
+ggplot(data = gap, aes(x = income, y = life_exp)) +  
+  geom_point(shape = 21, aes(fill = region, size = population)) +
+  scale_x_continuous(
+    name = "Per-person income (in international dollars)", 
+    # breaks = c(0.5, 1, 2, 4, 8, 16, 32, 64, 128),
+    labels = label_number(prefix = "$", suffix = "k")
+  ) +
+  scale_y_continuous(
+    name = "Life expectancy (in years)"
+  ) +
+  scale_fill_manual(
+    name = "World Region",
+    values = c("#87CEEB", "#DDA0DD", "#FFA500", "#7FFF00"),
+    labels = c("Africa", "The Americas", "Asia", "Europe")
+  ) +
+  scale_radius(range = c(1, 15)) +
+  theme_minimal() +
+  guides(size = "none") +
+  annotate(
+    geom = "text",
+    x = 40.5, #The x= and y= values give the coordinates for where the label should go
+    y = 84.8, #Finding the correct x and y coordinate is trial and error
+    label = "Japan", #The label= sets the text in your label
+    hjust = 1  #This right-aligns the text to the coordinate (40.5, 84.8)
+  )
+
+
+
 
